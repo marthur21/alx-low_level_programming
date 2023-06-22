ebc6b3e82eeff7dd0b7ab217994979b4edafa84e
Maxwell Arthur <maxwellarthur4196@gmail.com>
2023-06-21 19:06:06 -0700

    donme

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -6,8 +7,11 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
+
 int print_sign(int n)
+
 {
+	
 	if (n > 0)
 	{
 	_putchar(43);
